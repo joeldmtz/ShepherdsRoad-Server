209a07fbbd2b37cb97092ca484a541a71b93daa0
Joel De la O <joeldmtz.dev@gmail.com>
2015-08-10 17:21:21 -0600

    Cambiada la url de la db

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ var config = {
     },
 }
 
-config.db.host = (process.env.NODE_ENV == 'DEBUG')?'mongodb://localhost/' + 'ShepherdsRoad':'mongodb://'+ process.env.MONGO_USER +':'+ process.env.MONGO_PASS +'@ds031873.mongolab.com:31873/shepherds-road';
+config.db.host = (process.env.NODE_ENV == 'DEBUG')?'mongodb://localhost/' + 'ShepherdsRoad':'mongodb://'+ process.env.MONGO_USER +':'+ process.env.MONGO_PASS +'@ds031873.mongolab.com:31873/shepherdsroad';
 
 module.exports = config;
